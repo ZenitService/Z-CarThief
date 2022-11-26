3560d31815dffda9e5981d506e5766a8cb8f7d5e
GABRIELEYT <marologgpati@gmail.com>
2022-11-27 00:17:15 +0100

    Z-CarThief

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -2,6 +2,7 @@ author 'Zenit Service'
 description 'Free Relase Zenit Service Furto Veicolo '
 
 version '1.0.0'
+versioncheck 'https://github.com/ZenitService/Z-CarThief/blob/main/fxmanifest.lua'
 
 fx_version 'adamant'
 games {'gta5'}
@@ -10,7 +11,8 @@ lua54 'yes'
 server_scripts {
 	'@ox_lib/init.lua',
 	'config.lua',
-	'server/main.lua'
+	'server/main.lua',
+	'server/sv_version.lua'
 }
 
 client_scripts {
